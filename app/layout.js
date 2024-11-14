c0ccbd7fa542412441c070a9762ba0513bf4adf4
isaladwy <isaladwy@gmail.com>
2024-11-14 20:44:06 +0200

    Improving the Navigation and Root Layout

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from '@/app/_components/Navigation';
 
 import '@/app/_styles/globals.css';
 import { Josefin_Sans } from 'next/font/google';
+import Header from './_components/Header';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -25,14 +26,13 @@ function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} text-primary-100 min-h-screen bg-primary-950`}
+        className={`${josefin.className} antialiased text-primary-100 min-h-screen bg-primary-950 flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by the wild oasis</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
