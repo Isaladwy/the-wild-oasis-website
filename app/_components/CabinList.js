ee5676a5a3aa4f7b0e4c46175ec3038a934cf59e
isaladwy <isaladwy@gmail.com>
2024-12-18 20:41:54 +0200

    Caching and ISR

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,9 +1,12 @@
 import CabinCard from './CabinCard';
 import { getCabins } from '../_lib/data-service';
+import { unstable_noStore } from 'next/cache';
 
 async function CabinList() {
+  // unstable_noStore();
+
   const cabins = await getCabins();
-  
+
   if (!cabins.length) return null;
 
   return (
