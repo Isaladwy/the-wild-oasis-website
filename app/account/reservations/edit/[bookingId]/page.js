c74c8cca9ddb3c1980d0a01f9704e8eee5cf22af
Isaladwy <Isaladwy@gmail.com>
2024-12-28 21:41:51 +0200

    Updating a Reservation

--- app/account/reservations/edit/[bookingId]/page.js
+++ app/account/reservations/edit/[bookingId]/page.js
@@ -1,3 +1,4 @@
+import SubmitButton from '@/app/_components/SubmitButton';
 import { updateBooking } from '@/app/_lib/actions';
 import { getBooking, getCabin } from '@/app/_lib/data-service';
 
@@ -16,7 +17,7 @@ export default async function Page({ params }) {
         action={updateBooking}
         className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
       >
-        <input type='hidden' value={bookingId} name='bookingId'/>
+        <input type="hidden" value={bookingId} name="bookingId" />
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
           <select
@@ -49,9 +50,9 @@ export default async function Page({ params }) {
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update reservation
-          </button>
+          <SubmitButton pendingLabel="Updating">
+            Update Reservation
+          </SubmitButton>
         </div>
       </form>
     </div>
