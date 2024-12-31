edb4e6bffa1dc41211eeac291856f2619dbb10de
Isaladwy <Isaladwy@gmail.com>
2024-12-31 14:21:46 +0200

    Final touch

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-import logo from "@/public/logo.png";
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '@/public/logo.png';
 
 function Logo() {
   return (
@@ -13,7 +13,7 @@ function Logo() {
         width="60"
         alt="The Wild Oasis logo"
       />
-      <span className="text-xl font-semibold text-primary-100">
+      <span className="text-xl font-semibold hidden md:inline text-primary-100">
         The Wild Oasis
       </span>
     </Link>
